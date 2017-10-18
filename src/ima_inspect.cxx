f7ae2209830d4ae93caf14f72be38445e7d7a021
Matthias Gerstner <matthias.gerstner@suse.de>
2017-10-18 10:03:03 +0200

    ima_inspect: extended program structure to visit each requested file

--- src/ima_inspect.cxx
+++ src/ima_inspect.cxx
@@ -1,12 +1,26 @@
 // C++
 #include <iostream>
+#include <memory>
 
 // Linux
-#include <sys/xattr.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+// contains a few more preprocessor defines than sys/xattr.h
+#include <attr/xattr.h>
 
 // ima-inspect
 #include "ima_inspect.hxx"
 #include "ima_exceptions.hxx"
+
+// ima-evm-utils
+#include "imaevm.h"
+
+const std::array<std::string, 2> ImaInspect::m_attr_names = {
+	"security.ima",
+	"security.evm"
+};
 
 ImaInspect::ImaInspect() :
 	m_cmdline("This utility allows to display security.ima and "
@@ -26,8 +40,125 @@ void ImaInspect::run()
 {
 	for( const auto &file: m_arg_files.getValue() )
 	{
-		std::cout << file << std::endl;
+		try
+		{
+			std::cout << file << "\n\n";
+			inspectFile(file);
+		}
+		catch( const std::exception &ex )
+		{
+			std::cerr << ex.what() << std::endl;
+			m_res = 2;
+		}
 	}
+}
+
+void ImaInspect::inspectFile(const std::string &path)
+{
+	auto fd = open(path.c_str(), O_RDONLY | O_NOFOLLOW);
+
+	if( fd == -1 )
+	{
+		throw SysError("open");
+	}
+
+	for( const auto &attr: m_attr_names )
+	{
+		try
+		{
+			std::cout << attr << ": ";
+			bool have_attr = getAttr(fd, attr);
+
+			if( !have_attr )
+				continue;
+
+			inspectAttr();
+		}
+		catch( const std::exception &ex )
+		{
+			std::cerr << ex.what() << std::endl;
+			m_res = 2;
+		}
+	}
+
+	close(fd);
+}
+
+void ImaInspect::inspectAttr() const
+{
+	const auto type = static_cast<enum evm_ima_xattr_type>(
+		m_attr_data.at(0)
+	);
+
+	switch(type)
+	{
+	case IMA_XATTR_DIGEST:
+		//inspectDigest();
+		break;
+	case IMA_XATTR_DIGEST_NG:
+		//inspectDigestNg();
+		break;
+	case EVM_XATTR_HMAC:
+		//inspectHmac();
+		break;
+	case EVM_IMA_XATTR_DIGSIG:
+		inspectDigsig();
+		break;
+	default:
+		std::cout << "unknown IMA/EVM attribute tagged with "
+			<< static_cast<int>(type) << std::endl;
+		break;
+	}
+}
+
+void ImaInspect::inspectDigsig() const
+{
+	std::cout << "digital signature" << std::endl;
+}
+
+bool ImaInspect::getAttr(int fd, const std::string &attr)
+{
+	m_attr_data.clear();
+	int res = -1;
+
+	while( true )
+	{
+		res = fgetxattr(
+			fd,
+			attr.c_str(),
+			m_attr_data.data(),
+			m_attr_data.size()
+		);
+
+		if( res == -1 )
+		{
+			if( errno == ERANGE )
+			{
+				// re-calculate required space
+				m_attr_data.clear();
+				continue;
+			}
+			else if( errno == ENOATTR )
+			{
+				std::cout << "no such attribute\n";
+				// no attribute
+				return false;
+			}
+
+			throw SysError("fgetxattr");
+		}
+		else if( m_attr_data.empty() )
+		{
+			const auto length = static_cast<size_t>(res);
+			m_attr_data.resize(length);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return true;
 }
 
 int main(const int argc, const char **argv)
@@ -37,13 +168,12 @@ int main(const int argc, const char **argv)
 		ImaInspect ima_inspect;
 		ima_inspect.parseArgs(argc, argv);
 		ima_inspect.run();
-		return 0;
+		return ima_inspect.getRes();
 	}
 	catch( const std::exception &ex )
 	{
 		std::cerr << "Failed: " << ex.what() << std::endl;
+		return 1;
 	}
-
-	return 0;
 }
 
--- src/ima_inspect.hxx
+++ src/ima_inspect.hxx
@@ -7,6 +7,9 @@
 #ifndef IMA_INSPECT_HXX
 #define IMA_INSPECT_HXX
 
+// C++
+#include <array>
+
 // third-party
 #include <tclap/CmdLine.h>
 
@@ -20,8 +23,28 @@ public: // functions
 
 	void run();
 
+	int getRes() const { return m_res; }
+
+protected: // functions
+
+	void inspectFile(const std::string &path);
+
+	bool getAttr(int fd, const std::string &attr);
+
+	void inspectAttr() const;
+
+	void inspectDigsig() const;
+
 protected: // data
 
+	int m_res = 0;
+	//! holds the currently handled xattr value
+	std::vector<char> m_attr_data;
+
+	// the xattr names to inspect per file
+	static const std::array<std::string, 2> m_attr_names;
+
+	// command line parsing
 	TCLAP::CmdLine m_cmdline;
 	TCLAP::UnlabeledMultiArg<std::string> m_arg_files;
 };
